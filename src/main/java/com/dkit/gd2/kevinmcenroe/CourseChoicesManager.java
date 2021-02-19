218bd02c10c9ee06273edde3d2baa10305657ac5
Kevin McEnroe <d00242092@student.dkit.ie>
2021-02-19 21:34:40 +0000

    Developed methods to retrieve student/course data
    Developed methods to return student and course details per caoNumber and
    courseID respectively. Wrote pseudocode for the outstanding methods in
    CourseChoicesManager to inform later development.

--- src/main/java/com/dkit/gd2/kevinmcenroe/CourseChoicesManager.java
+++ src/main/java/com/dkit/gd2/kevinmcenroe/CourseChoicesManager.java
@@ -10,6 +10,9 @@ package com.dkit.gd2.kevinmcenroe;
 //
 // Clone all received and returned objects - encapsulation
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class CourseChoicesManager extends StudentManager{
 
     // reference to constructor injected studentManager
@@ -19,9 +22,10 @@ public class CourseChoicesManager extends StudentManager{
     private CourseManager courseManager;
 
     // Store all the Course details -  fast access
+    HashMap<String, Course> courseDetails = new HashMap<String, Course>();
 
     // caoNumber, course selection list - for fast access
-
+    HashMap<String, ArrayList<Course>> courseChoices = new HashMap<String, ArrayList<Course>>();
 
     // CourseChoicesManager DEPENDS on both the StudentManager and CourseManager to access
     // student details and course details.  So, we receive a reference to each via
@@ -35,23 +39,58 @@ public class CourseChoicesManager extends StudentManager{
 
     }
 
-//    public Student getStudentDetails() {
-//    }
-//
-//    public getCourseDetails() {
-//    }
-//
-//    public  getStudentChoices() {
-//    }
-//
-//    void updateChoices() {
-//    }
-//
-//    public  getAllCourses() {
-//    }
-//
-//    boolean login() {
-//    }
+    public Student getStudentDetails(int caoNumber) {
+        Student matchingStudent = null;
+        if(studentManager.studentsMap.containsKey(caoNumber))
+            matchingStudent = studentManager.studentsMap.get(caoNumber);
+        else{
+            System.out.println("A student of CAO number " + caoNumber + " does not exist in the student map");
+        }
+        return matchingStudent;
+    }
+
+    public Course getCourseDetails(String courseID) {
+        Course matchingCourse = null;
+        if(courseManager.coursesMap.containsKey(courseID))
+            matchingCourse = courseManager.coursesMap.get(courseID);
+        else{
+            System.out.println("A course of ID " + courseID + " does not exist in the course map");
+        }
+        return matchingCourse;
+    }
+
+    //Pseudocode below is not final. This is my initial breakdown.
+
+    public void getStudentChoices(){
+        //TO DO - READ BRIEF
+        //Maybe take input from a menu first
+        //Feed that into a map of key caoNumber
+        //Then search for a match in the map
+    }
+
+    void updateChoices() {
+        //TO DO - READ BRIEF
+        //Search in map for matching caoNumber provided
+        //Get reference to the courseChoices of that student
+        //Take input/updated values from menu
+        //Overwrite existing data in map with new values provided
+        //Handle exceptions!
+    }
+
+    public void getAllCourses() {
+        //TO DO - READ BRIEF
+        //Has to return a LIST of all courses
+        //Cycle through courseDetails map perhaps
+        //Return their data, maybe output it too
+    }
+
+    boolean login() {
+        //TO DO - READ BRIEF
+        //Take input via menu for caoNumber, dateOfBirth etc
+        //Check if they are a perfect match with any of the students in students.dat
+        //If there is a match, allow the menu system to continue maybe
+        return true;
+    }
 
 
 }
